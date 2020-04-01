4c55a049daeec0bb714242054401d121ca1a9aa3
Simon <simon27@protonmail.ch>
2020-04-01 15:23:32 +0200

    Modals for gallery and contact tab

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// MAKE TABS CLICKABLE
+
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -15,3 +17,40 @@ function openTab(evt, tabName) {
 }
 
 document.getElementById("defaultOpen").click();
+
+
+// MODALS FOR PHOTO GALLERY
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function openModal(img) {
+  // Get the image and insert it inside the modal - use 
+  // its "alt" text as a caption
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, 
+// close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const imgs = document.querySelectorAll('.gallery-photo > img');
+[...imgs].forEach(img => {
+    img.addEventListener('click', e => { openModal(e.target); });
+});
