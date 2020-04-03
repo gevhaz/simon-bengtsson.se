90588d1fd808210e233dedfe027a06ccb45bf1ce
Simon <simon27@protonmail.ch>
2020-04-03 19:18:18 +0200

    Thumbnails, adjustments to modal size and small edits

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ document.getElementById("defaultOpen").click();
 
 // Get the modal
 var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
+var modalImg = document.getElementById("modal-img");
 var captionText = document.getElementById("caption");
 
 // Get the <span> element that closes the modal
@@ -33,13 +33,14 @@ function openModal(img) {
   // Get the image and insert it inside the modal - use 
   // its "alt" text as a caption
   modal.style.display = "block";
-  modalImg.src = img.src;
+  modalImg.src = img.src.replace("thumbnails", "fullsize");
   captionText.innerHTML = img.alt;
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  modalImg.src = "";
 }
 
 // When the user clicks anywhere outside of the modal, 
@@ -47,6 +48,7 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    modalImg.src = "";
   }
 }
 
